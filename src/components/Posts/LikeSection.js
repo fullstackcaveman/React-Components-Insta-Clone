d5ca24ede3bba94aa821383b52c7c976f84b4435
Chris Burton <chris@fullstackcaveman.com>
2021-02-16 15:09:16 -0700

    Fixed Liked Section. Added 'likes' after number of likes

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -20,7 +20,7 @@ const LikeSection = (props) => {
 					<FontAwesomeIcon icon={faComment} />
 				</div>
 			</div>
-			<p className='like-number'>{numberOfLikes}</p>
+			<p className='like-number'>{numberOfLikes} likes</p>
 		</div>
 	);
 };
